7f88e7673ce5c368f3f66d8419e4b1c87704705f
Riccardo V. Vincelli <rvvincelli@github.com>
2021-09-08 13:21:32 +0300

    Introduces a collections gateway

---
+++ src/Gateway/CollectionGateway.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+namespace Twikey\Api\Gateway;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use Twikey\Api\TwikeyException;
+
+class CollectionGateway extends BaseGateway
+{
+
+    /**
+     * @throws TwikeyException
+     * @throws ClientExceptionInterface
+     */
+    public function create(int $ct, ?string $colltndt, ?array $mndtId, ?bool $prenotify, $lang = 'en')
+    {
+        $response = $this->request('POST', sprintf('/creditor/collect?ct=%s%s%s%s', $ct, isset($colltndt) ? "&colltndt=$colltndt" : null, isset($mndtId) && !empty($mndtId) ? sprintf("&mndtId=%s", implode (',', $mndtId)) : null, isset($prenotify) ? sprintf("&prenotify=%s", $prenotify ? 'true' : 'false') : null), [], $lang);
+        $server_output = $this->checkResponse($response, "Executing a collection!");
+        return json_decode($server_output);
+    }
+
+    /**
+     * @throws TwikeyException
+     * @throws ClientExceptionInterface
+     */
+    public function get(?int $id, ?string $pmtinfid, $lang = 'en')
+    {
+        $response = $this->request('GET', sprintf('/creditor/collect%s%s%s', isset($id) || isset($pmtinfid) ? '?' : null, isset($id) ? "id=$id" : null, isset($pmtinfid) ? sprintf("%spmtinfid=$pmtinfid", isset($id) ? '&' : null) : null, [], $lang));
+        $server_output = $this->checkResponse($response, "Retrieving collections!");
+        return json_decode($server_output);
+    }
+
+}
