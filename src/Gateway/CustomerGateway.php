c4586014624e5d6a62e921ecac7d26822e75ce28
Bonno <bonno@ecomni.nl>
2022-09-29 16:25:51 +0200

    Add CustomerGateway to provide for updating a customer

---
+++ src/Gateway/CustomerGateway.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Twikey\Api\Gateway;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use Twikey\Api\Exception\TwikeyException;
+
+class CustomerGateway extends BaseGateway
+{
+    /**
+     * @throws TwikeyException
+     * @throws ClientExceptionInterface
+     */
+    public function update($data)
+    {
+        if (!isset($data['customerNumber'])) {
+            throw new TwikeyException("A customerNumber is required");
+        }
+        $response = $this->request(
+            'PATCH',
+            \sprintf("creditor/customer/%s", $data['customerNumber']),
+            ['query' => $data]
+        );
+        $server_output = $this->checkResponse($response, "Update a customer!");
+        return json_decode($server_output);
+    }
+}
